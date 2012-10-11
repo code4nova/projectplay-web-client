883eab3b646f74b1b4380afabf68de0665af2125
Dan <ddombrow@gmail.com>
2012-10-11 18:16:55 -0400

    Switched to Bootstrap instead of jquery-ui, implemented simple geocoding.

--- js/app.js
+++ js/app.js
@@ -34,17 +34,83 @@ var playApp = function()
 	};
 
 	var initSidebar = function() {
-		//TODO: lolololol spaces, need to find a non hack way to get the button to look right
-		$("#app_panel").append("<a id='showAllBtn' class='app_panel_button' href='#'>Show all playgrounds...</a>").button().click(
+		
+		$("#showAllBtn").click(
 			function() {
 				showAllPlaygrounds();
 			}
 		);
+
+		$("#addressSearchBtn").click(
+			function() {
+				searchByAddress();
+			}
+		);
+
+		/*$("#search").append("<div><input id='loc_auto' type='text' /></div>");
+
+		var input = document.getElementById('searchTextField');
+        var autocomplete = new google.maps.places.Autocomplete(input);
+        autocomplete.bindTo('bounds', inst.map);
+        var infowindow = new google.maps.InfoWindow();
+        var marker = new google.maps.Marker({
+          map: inst.map
+        });
+
+        google.maps.event.addListener(autocomplete, 'place_changed', function() {
+          infowindow.close();
+          var place = autocomplete.getPlace();
+          if (place.geometry.viewport) {
+            inst.map.fitBounds(place.geometry.viewport);
+          } else {
+            inst.map.setCenter(place.geometry.location);
+            inst.map.setZoom(17);  // Why 17? Because it looks good.
+          }
+
+          var image = new google.maps.MarkerImage(
+              place.icon,
+              new google.maps.Size(71, 71),
+              new google.maps.Point(0, 0),
+              new google.maps.Point(17, 34),
+              new google.maps.Size(35, 35));
+          marker.setIcon(image);
+          marker.setPosition(place.geometry.location);
+
+          var address = '';
+          if (place.address_components) {
+            address = [
+              (place.address_components[0] && place.address_components[0].short_name || ''),
+              (place.address_components[1] && place.address_components[1].short_name || ''),
+              (place.address_components[2] && place.address_components[2].short_name || '')
+            ].join(' ');
+          }
+
+          infowindow.setContent('<div><strong>' + place.name + '</strong><br>' + address);
+          infowindow.open(inst.map, marker);
+        });*/
 	};
 
 	var showAllPlaygrounds = function() {
 		inst.svc.getAllPlaygrounds(renderPlaygrounds);
 	};
+
+	var searchByAddress = function() {
+		var address = $('#inputAddress').val();
+		var dist = $('#inputDistance').val();
+		inst.svc.geoCodeAddress(address, renderAddressSearch);
+	}
+
+	var renderAddressSearch = function(results) {
+		console.log(results);
+		var pt = new google.maps.LatLng(results.geometry.location.lat(), results.geometry.location.lng());
+		var name = results.formatted_address;
+		var marker = new google.maps.Marker({
+		            position: pt,
+		            map: inst.map,
+		            title: name,
+		            //icon: image
+		        });
+	}
 
 	var renderPlaygrounds = function(playData) {
 		if (playData) {
@@ -52,7 +118,11 @@ var playApp = function()
 				var playObj = playData[i];
 				console.log(playObj);
 
-				var image = new google.maps.MarkerImage('./img/playground_marker.png');
+				var image = new google.maps.MarkerImage('./img/playground_marker.png',
+						new google.maps.Size(32, 37), //icon size
+					    new google.maps.Point(0,0), //origin
+					    new google.maps.Point(16, 37) //offset
+					);
 				var pt = new google.maps.LatLng(playObj.lat, playObj.long);
 				var marker = new google.maps.Marker({
 		            position: pt,
--- js/playSvc.js
+++ js/playSvc.js
@@ -20,5 +20,35 @@ var playSvc = function(url) {
 		});
 	};
 
+	inst.geoCodeAddress = function(address, callback) {
+		var caller = callback;
+		/*var geoCodeBaseUrl = "https://maps.googleapis.com/maps/api/geocode/json?";
+		var gcUrl = [geoCodeBaseUrl, "address=", encodeURIComponent(address), "&sensor=false"].join("");
+		
+		//console.log(gcUrl);
+		$.ajax({
+			type: 'GET',
+			url: gcUrl,
+			async: false,
+			contentType: 'application/json',
+			dataType: 'jsonp',
+			success: function(json) {
+				caller.apply(null, [json]);
+		    },
+		    error: function(e) {
+		       caller.apply(null, [false]);
+		    }
+		});*/
+		var geocoder = new google.maps.Geocoder(); 
+		geocoder.geocode(
+		    { address : address, 
+		      region: 'no' 
+		    }, 
+		    function(results, status){
+		      caller.apply(null, [results[0]]);
+			}
+		);
+	};
+
 	return inst;
 }

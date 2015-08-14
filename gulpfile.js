ffeed392b4faa6447e4d20d0aadce8227dd0d49d
Robbie Holmes <robbie@phase2technology.com>
2015-08-14 13:27:44 -0400

    Completed the initial optimization.
    This includes the gulp process for compiling the files, the newly added
    ico file, and the optimized background image.

--- gulpfile.js
+++ gulpfile.js
@@ -3,13 +3,24 @@ var gulp = require("gulp");
 var concat = require("gulp-concat");
 var uglify = require("gulp-uglify");
 var rename = require("gulp-rename");
+var watch = require("gulp-watch");
 
+
+var files = [ "js/jquery.js", "js/jquery-migrate.min.js",
+  "js/modernizr.custom.06523.js", "js/site-wide.js", "js/wait-for-images.js",
+  "js/background-image.js", "js/jquery.form.min.js",
+  "js/jquery.simplemodal.1.4.4.min.js", "js/scripts.js", "js/playSvc.js",
+  "js/app.js", "js/initialize.js"];
 // Concatenate & Minify JS
 gulp.task('scripts', function() {
-    return gulp.src('js/*.js')
+    return gulp.src(files)
         .pipe(concat('all-scripts.js'))
         .pipe(gulp.dest('js'))
         .pipe(rename('script.js'))
         .pipe(uglify())
         .pipe(gulp.dest('js'));
+});
+
+gulp.task('watch', function(){
+    return gulp.watch(files, ['scripts']);
 });
